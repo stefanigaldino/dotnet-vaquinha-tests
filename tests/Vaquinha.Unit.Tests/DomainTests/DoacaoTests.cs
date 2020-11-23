4c0ce04789a9190a46659a4195e3c9128279d45d
thabcm <thalitahelp@gmail.com>
2020-11-23 15:22:14 -0300

    Implementa novo teste para funcionalidade

--- tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
+++ tests/Vaquinha.Unit.Tests/DomainTests/DoacaoTests.cs
@@ -38,6 +38,24 @@ namespace Vaquinha.Unit.Tests.DomainTests
         }
 
         [Fact]
+        [Trait("Doacao", "Doacao_CorretamentePreenchidos_DoacaoValida")]
+        public void Doacao_UsuarioAceitaPagarComTaxa_DoacaoValida()
+        {           
+            // Arrange
+            var doacao = _doacaoFixture.DoacaoValida(false, 5, false, true);
+            doacao.AdicionarEnderecoCobranca(_enderecoFixture.EnderecoValido());
+            doacao.AdicionarFormaPagamento(_cartaoCreditoFixture.CartaoCreditoValido());
+
+            // Act
+            var valido = doacao.Valido();
+
+            // Assert
+            valido.Should().BeTrue(because: "os campos foram preenchidos corretamente");
+            doacao.Valor.Should().Be(6, because: "valor com taxa de 20%");
+            doacao.ErrorMessages.Should().BeEmpty();
+        }
+
+        [Fact]
         [Trait("Doacao", "Doacao_DadosPessoaisInvalidos_DoacaoInvalida")]
         public void Doacao_DadosPessoaisInvalidos_DoacaoInvalida()
         {
